89823e657ee80b6215b3747a03c88759eb6ed5e3
ELCOT <ELCOT@Lenovo>
2022-01-29 06:35:52 -0800

    typecast modified

--- basic java concepts/src/arrayinput.java
+++ basic java concepts/src/arrayinput.java
@@ -1,4 +1,19 @@
-
+import java.util.Scanner;
 public class arrayinput {
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		int[] array=new int[10];
+		int sum=0;
+		System.out.println("Enter the elements:");
+		for (int i=0;i<10;i++)
+		{
+			array[i]=input.nextInt();
+		}
+		for (int num:array) {
+			sum=sum+num;
+		}
+		System.out.println("Sum of array elements is:"+sum);
+		
+	}
 
 }
--- basic java concepts/src/typecast.java
+++ basic java concepts/src/typecast.java
@@ -1,5 +1,12 @@
 
 public class typecast {
-	
-
+	public static void main(String[] args) {
+		byte b=10;
+		int i=b;
+		byte c=i;
+		byte d=(byte)i;
+		byte x=10;
+		byte y=20;
+		byte sum=x*y;
+	}
 }
